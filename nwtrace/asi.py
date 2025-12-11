fa439153ab493aeb66e3ba9126e9496baff0bc53
ga-holmes <garrett.a.holmes01@gmail.com>
2025-12-11 17:14:18 -0500

    finished minor code implementation of ASI, untestes

--- nwtrace/asi.py
+++ nwtrace/asi.py
@@ -4,7 +4,30 @@ import numpy as np
 # Defining globals here (will likely be an object later)
 
 # A set representing outfall-inlet connections
-outfall_inlet_connections = {}
+inlet_connections = {
+    "OF1": ["IN1"]
+}
+
+outfall_locations = {
+    (8,1): "OF1"
+}
+
+inlet_locations = {
+    "IN1": (2,2)
+}
+
+# d8 direction to vector map for whitebox tools encoding
+direction_map = {
+    0:   (0,0),   # Sink
+    1:   (-1,1),  # NE
+    2:   (0,1),   # E
+    4:   (1,1),    # SE
+    8:   (1,0),     # S
+    16:  (1,-1),    # SW
+    32:  (0,-1),     # W
+    64:  (-1,-1),    # NW
+    128: (-1,0)      # N
+}
 
 # sample DEM from paper
 dem = np.array([
@@ -20,6 +43,18 @@ dem = np.array([
     [55, 53, 61, 80, 80, 80, 70, 62, 66, 67],   # y = 10
 ], dtype=np.float32)
 
+# create the outfall mask that indicates the locations of outfalls in the dataset
+outfall_mask = np.zeros_like(dem)
+
+for r,c in outfall_locations.keys():
+    outfall_mask[r,c] = 1
+
+# create an inlet mask that indicates the locations of inlets in the dataset so they can be excluded from the list of seed cells
+inlet_mask = np.zeros_like(dem)
+
+for r,c in inlet_locations.keys():
+    inlet_mask[r,c] = 1
+
 # placeholders for rasters
 # D8 flow direction raster
 d8_dir = np.empty()
@@ -31,25 +66,61 @@ d8_watershed = np.empty()
 # set option for watershed type (Options: "OUTLET", "INLET", "OUTFALL")
 watershed = "OUTFALL"
 
+# global raster extent maximums
+max_rows = np.max(dem,axis=0)
+max_cols = np.max(dem,axis=1)
+
 # Returns cell coordinates for cells that drain into (x, y) of the D8 flow direction raster
-def get_d8_neighbours(x, y) -> list:
+def get_d8_neighbours(r, c) -> list:
 
     # List of tuples
     neighbours = []
 
-    # find inflow to (x,y)
+    # this is a static list, i.e. always the same
+    all_neighbours = [
+        (-1,-1), (-1,0), (-1,1),
+        (0,-1),          (0,1),
+        (1,-1),  (1,0),  (1,1)
+    ]
+
+    # for all cells
+    for dr, dc in all_neighbours:
+
+        nr = r + dr
+        nc = c + dc
+
+        # Skip cells outside raster
+        if nr < 0 or nc < 0 or nr or nr > max_rows or nr > max_cols:
+            continue
+
+        n = d8_dir[nr, nc]
+
+        # get the vector value for the d8 number at n
+        dn = direction_map[n]
+
+        # check for inflow to (x,y)
+        if dn == ((-1 * dr), (-1 * dc)):
+            
+            o_id = None
+            # check if it's an outfall, if yes, assign outfall ID
+            if outfall_mask[dn] == 1:
+                o_id = outfall_locations[dn]
+
+            neighbours.append((nr, nc, o_id))
 
     return neighbours
 
-# Returns the cell coordinates for inlets that are connected to outfall 'id' in the connectivity table
-def get_connecting_inlets(id: str) -> list:
+def get_seed_cells(d8_dir, to_exclude = None):
+    
+    if to_exclude != None:
+        sink_mask = (d8_dir == 0) & (~to_exclude)
+    else: 
+        sink_mask = (d8_dir == 0)
 
-    # List of tuples
-    inlets = []
+    sink_cells = [tuple(rc) for rc in np.argwhere(sink_mask)]
 
-    # traverse connectivity table for connecting inlets to outfall 'id'
+    return sink_cells
 
-    return inlets
 
 def recursive_asi(x, y, id: str, o_od: str = None):
     # x and y are the locations of 'seed' cells from which to initiate the recursive process
@@ -69,9 +140,11 @@ def recursive_asi(x, y, id: str, o_od: str = None):
         d8_accum[x, y] = d8_accum[x, y] + d8_accum[i, j] + 1
 
     # If cell (x,y) is an outfall:
-    if o_id is not None and "OF" in o_id:
-        for (p,q,i_id) in get_connecting_inlets(x,y):
-            
+    if o_id is not None:
+        for i_id in inlet_connections[o_id]:
+
+            p, q = inlet_locations[i_id]
+
             # placeholder for a potentially better system later
             match watershed:
                 case "OUTLET":
@@ -87,3 +160,5 @@ def recursive_asi(x, y, id: str, o_od: str = None):
     
     # assign watershed value
     d8_watershed[x, y] = id
+
+# testing the whole thing
