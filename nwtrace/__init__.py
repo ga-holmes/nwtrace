aec29c1f9d944dcc1e9b333bf27ba639e1802660
ga-holmes <garrett.a.holmes01@gmail.com>
2025-12-11 18:07:03 -0500

    added asi to init, refactored to class form, made simple test notebook (not clear if it works)

--- nwtrace/__init__.py
+++ nwtrace/__init__.py
@@ -1,5 +1,6 @@
 # Import the submodules
 from .trace_sewershed import NWTrace
+from .asi import ASI
 from .utils import (
     dfs,
     dfs_directed,
@@ -8,6 +9,6 @@ from .utils import (
 )
 
 # Define the __all__ variable
-__all__ = [NWTrace, utils]
+__all__ = [NWTrace, ASI, utils]
 
 version = "0.0.1"
--- nwtrace/asi.py
+++ nwtrace/asi.py
@@ -1,164 +1,159 @@
 # Adaptive Stormwater Infrastructure (ASI) Algorithm, adapted from Choi et al. (2011) : 10.1016/j.cageo.2010.07.008
 import numpy as np
+from pathlib import Path
+import rasterio as rio
 
 # Defining globals here (will likely be an object later)
 
-# A set representing outfall-inlet connections
-inlet_connections = {
-    "OF1": ["IN1"]
-}
+class ASI:
 
-outfall_locations = {
-    (8,1): "OF1"
-}
+    def __init__(
+        self,
+        d8_flow_direction_raster,
+        inlet_connections,
+        outfall_locations,
+        inlet_locations,
+        watershed_type = "OUTFALL",
+        direction_map = None
 
-inlet_locations = {
-    "IN1": (2,2)
-}
+    ) -> None:
 
-# d8 direction to vector map for whitebox tools encoding
-direction_map = {
-    0:   (0,0),   # Sink
-    1:   (-1,1),  # NE
-    2:   (0,1),   # E
-    4:   (1,1),    # SE
-    8:   (1,0),     # S
-    16:  (1,-1),    # SW
-    32:  (0,-1),     # W
-    64:  (-1,-1),    # NW
-    128: (-1,0)      # N
-}
+        # load d8 file using rasterio or assign
+        if isinstance(d8_flow_direction_raster, (str, Path)):
+            with rio.open(d8_flow_direction_raster) as src:
+                self.d8_dir = src.read(1)
+        elif isinstance(d8_flow_direction_raster, np.ndarray):
+            self.d8_dir = d8_flow_direction_raster
+        elif isinstance(d8_flow_direction_raster, (rio.io.DatasetReader)):
+            self.d8_dir = d8_flow_direction_raster.read(1)
+        else:
+            raise ValueError(
+                "'d8_flow_direction_raster' must be a valid file path or a numpy array or a rasterio DatasetReader."
+            )
 
-# sample DEM from paper
-dem = np.array([
-    [98, 98, 99, 95, 94, 86, 92, 96, 99, 102],  # y = 1
-    [89, 64, 62, 62, 89, 91, 89, 98, 98, 100],  # y = 2
-    [85, 53, 51, 59, 80, 90, 88, 82, 92, 96],   # y = 3
-    [89, 84, 72, 64, 67, 70, 80, 87, 94, 94],   # y = 4
-    [84, 85, 92, 96, 94, 90, 81, 79, 81, 92],   # y = 5
-    [72, 82, 87, 92, 92, 83, 78, 84, 76, 90],   # y = 6
-    [64, 64, 66, 69, 79, 88, 84, 78, 84, 88],   # y = 7
-    [72, 80, 77, 69, 62, 70, 83, 79, 78, 84],   # y = 8
-    [71, 79, 80, 80, 77, 64, 62, 67, 70, 76],   # y = 9
-    [55, 53, 61, 80, 80, 80, 70, 62, 66, 67],   # y = 10
-], dtype=np.float32)
+        # TODO: May update this to a better system that gets all this info from a single array or table such as in the Choi paper
+        self.inlet_connections = inlet_connections
+        self.outfall_locations = outfall_locations
+        self.inlet_locations = inlet_locations
 
-# create the outfall mask that indicates the locations of outfalls in the dataset
-outfall_mask = np.zeros_like(dem)
+        self.watershed_type = watershed_type
+        
+        if direction_map == None:
+            # d8 direction to vector map for whitebox tools encoding
+            self.direction_map = {
+                0:   (0,0),   # Sink
+                1:   (-1,1),  # NE
+                2:   (0,1),   # E
+                4:   (1,1),    # SE
+                8:   (1,0),     # S
+                16:  (1,-1),    # SW
+                32:  (0,-1),     # W
+                64:  (-1,-1),    # NW
+                128: (-1,0)      # N
+            }
 
-for r,c in outfall_locations.keys():
-    outfall_mask[r,c] = 1
+        self.max_rows = np.shape(self.d8_dir)[0]
+        self.max_cols = np.shape(self.d8_dir)[1]
 
-# create an inlet mask that indicates the locations of inlets in the dataset so they can be excluded from the list of seed cells
-inlet_mask = np.zeros_like(dem)
+        # initialize empty arrays for watershed and accumulation
+        self.d8_accum = np.zeros_like(self.d8_dir)
+        self.d8_watershed = np.full_like(self.d8_dir,"", dtype=object) # array of objects that can be strings later
 
-for r,c in inlet_locations.keys():
-    inlet_mask[r,c] = 1
+        # create the outfall mask that indicates the locations of outfalls in the dataset
+        self.outfall_mask = np.zeros_like(self.d8_dir)
 
-# placeholders for rasters
-# D8 flow direction raster
-d8_dir = np.empty()
-# D8 flow accumulation raster
-d8_accum = np.empty()
-# Watershed assignment raster
-d8_watershed = np.empty()
+        for r,c in outfall_locations.keys():
+            self.outfall_mask[r,c] = 1
+        
 
-# set option for watershed type (Options: "OUTLET", "INLET", "OUTFALL")
-watershed = "OUTFALL"
+    # Returns cell coordinates for cells that drain into (x, y) of the D8 flow direction raster
+    def get_d8_neighbours(self, r, c) -> list:
 
-# global raster extent maximums
-max_rows = np.max(dem,axis=0)
-max_cols = np.max(dem,axis=1)
+        # List of tuples
+        neighbours = []
 
-# Returns cell coordinates for cells that drain into (x, y) of the D8 flow direction raster
-def get_d8_neighbours(r, c) -> list:
+        # this is a static list, i.e. always the same
+        all_neighbours = [
+            (-1,-1), (-1,0), (-1,1),
+            (0,-1),          (0,1),
+            (1,-1),  (1,0),  (1,1)
+        ]
 
-    # List of tuples
-    neighbours = []
+        # for all cells
+        for dr, dc in all_neighbours:
 
-    # this is a static list, i.e. always the same
-    all_neighbours = [
-        (-1,-1), (-1,0), (-1,1),
-        (0,-1),          (0,1),
-        (1,-1),  (1,0),  (1,1)
-    ]
+            nr = r + dr
+            nc = c + dc
 
-    # for all cells
-    for dr, dc in all_neighbours:
+            # Skip cells outside raster extent
+            if nr < 0 or nr >= self.max_rows or nc < 0 or nc >= self.max_cols:
+                continue
 
-        nr = r + dr
-        nc = c + dc
+            n = self.d8_dir[nr, nc]
 
-        # Skip cells outside raster
-        if nr < 0 or nc < 0 or nr or nr > max_rows or nr > max_cols:
-            continue
+            # get the vector value for the d8 number at n
+            dn = self.direction_map[n]
 
-        n = d8_dir[nr, nc]
+            # check for inflow to (x,y)
+            if dn == ((-1 * dr), (-1 * dc)):
+                
+                o_id = None
+                # check if it's an outfall, if yes, assign outfall ID
+                if self.outfall_mask[dn] == 1:
+                    o_id = self.outfall_locations[dn]
 
-        # get the vector value for the d8 number at n
-        dn = direction_map[n]
+                neighbours.append((nr, nc, o_id))
 
-        # check for inflow to (x,y)
-        if dn == ((-1 * dr), (-1 * dc)):
-            
-            o_id = None
-            # check if it's an outfall, if yes, assign outfall ID
-            if outfall_mask[dn] == 1:
-                o_id = outfall_locations[dn]
+        return neighbours
 
-            neighbours.append((nr, nc, o_id))
+    def get_seed_cells(self, d8_dir, to_exclude = None):
+        
+        if to_exclude is not None:
+            sink_mask = (d8_dir == 0) & (~to_exclude)
+        else: 
+            sink_mask = (d8_dir == 0)
 
-    return neighbours
+        sink_cells = [tuple(rc) for rc in np.argwhere(sink_mask)]
 
-def get_seed_cells(d8_dir, to_exclude = None):
-    
-    if to_exclude != None:
-        sink_mask = (d8_dir == 0) & (~to_exclude)
-    else: 
-        sink_mask = (d8_dir == 0)
-
-    sink_cells = [tuple(rc) for rc in np.argwhere(sink_mask)]
-
-    return sink_cells
+        return sink_cells
 
 
-def recursive_asi(x, y, id: str, o_od: str = None):
-    # x and y are the locations of 'seed' cells from which to initiate the recursive process
-    # These are edge cells, or inlets with the 'undefined' indicator
-    # Each seed cell will need to have a unique ID
+    def recursive_asi(self, r: int, c: int, id: str, o_id: str = None):
+        # x and y are the locations of 'seed' cells from which to initiate the recursive process
+        # These are edge cells, or inlets with the 'undefined' indicator
+        # Each seed cell will need to have a unique ID
 
-    # ------------------------
-    # The DEM will need to be modified to create intentional sinks at the locations of inlets
-    # These sinks will then be given an indicator for use in the procedure
+        # ------------------------
+        # The DEM will need to be modified to create intentional sinks at the locations of inlets
+        # These sinks will then be given an indicator for use in the procedure
 
-    # for every neighbour of (x,y)
-    for (i,j, o_id) in get_d8_neighbours(x, y):
-        # call this function on the current cell, id is propagated for assigning the watershed
-        recursive_asi(i,j,id,o_id)
+        # for every neighbour of (x,y)
+        for (i,j, o_id) in self.get_d8_neighbours(r, c):
+            # call this function on the current cell, id is propagated for assigning the watershed
+            self.recursive_asi(i,j,id,o_id)
 
-        # at the inlet cell, set to the current value plus the accumulated flow to cell (i,j) - +1 for the flow to this cell
-        d8_accum[x, y] = d8_accum[x, y] + d8_accum[i, j] + 1
+            # at the inlet cell, set to the current value plus the accumulated flow to cell (i,j) - +1 for the flow to this cell
+            self.d8_accum[r, c] = self.d8_accum[r, c] + self.d8_accum[i, j] + 1
 
-    # If cell (x,y) is an outfall:
-    if o_id is not None:
-        for i_id in inlet_connections[o_id]:
+        # If cell (x,y) is an outfall:
+        if o_id is not None:
+            for i_id in self.inlet_connections[o_id]:
 
-            p, q = inlet_locations[i_id]
+                p, q = self.inlet_locations[i_id]
 
-            # placeholder for a potentially better system later
-            match watershed:
-                case "OUTLET":
-                    w_id = id
-                case "OUTFALL":
-                    w_id = o_id
-                case "INLET":
-                    w_id = i_id
+                # placeholder for a potentially better system later
+                match self.watershed_type:
+                    case "OUTLET":
+                        w_id = id
+                    case "OUTFALL":
+                        w_id = o_id
+                    case "INLET":
+                        w_id = i_id
 
-            recursive_asi(p, q, w_id)
+                self.recursive_asi(p, q, w_id)
 
-            d8_accum[x, y] = d8_accum[x, y] + d8_accum[p, q] + 1
-    
-    # assign watershed value
-    d8_watershed[x, y] = id
+                self.d8_accum[r, c] = self.d8_accum[r, c] + self.d8_accum[p, q] + 1
+        
+        # assign watershed value
+        self.d8_watershed[r, c] = id
 
-# testing the whole thing
