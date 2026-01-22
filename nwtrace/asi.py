6ea575623c1430f8c3f89fc2ab1756a8f703d5a0
ga-holmes <garrett.a.holmes01@gmail.com>
2026-01-22 15:52:05 -0500

    made some optimizations to iterative_ASI algorithm

--- nwtrace/asi.py
+++ nwtrace/asi.py
@@ -14,7 +14,8 @@ class ASI:
         outfall_locations: dict,
         inlet_locations: dict,
         watershed_type: str = "OUTFALL",
-        direction_map: dict = None
+        direction_map: dict = None,
+        no_data_value: int = -32768
 
     ) -> None:
         """
@@ -53,6 +54,8 @@ class ASI:
                     int: (-1,0)     # N
                 } 
             , by default None
+        no_data_value: int, optional
+            The NoData integer set for the supplied d8_direction raster, by default -32768
 
         Raises
         ------
@@ -98,16 +101,29 @@ class ASI:
         self.max_cols = np.shape(self.d8_dir)[1]
 
         # initialize empty arrays for watershed and accumulation
-        self.d8_accum = np.zeros_like(self.d8_dir)
-        self.d8_watershed = np.zeros_like(self.d8_dir) # array of ints assigned sequentially, then realted to an ID table
-        self.watershed_table = dict()
+        self._init_accumulation_rasters()
 
         # create the outfall mask that indicates the locations of outfalls in the dataset
         self.outfall_mask = np.zeros_like(self.d8_dir)
+        
+        # set the nodata value if included
+        self.nodata_value = no_data_value
 
         for r,c in outfall_locations.keys():
             self.outfall_mask[r,c] = 1
+    
+    def _init_accumulation_rasters(self):
+        """
+        modular raster initialization so it can be repeated when the accumulation is re-calculated
+        """
+        # initialize empty arrays for watershed and accumulation
+        self.d8_accum = np.zeros_like(self.d8_dir, dtype=np.int32)
+        self.d8_watershed = np.zeros_like(self.d8_dir) # array of ints assigned sequentially, then realted to an ID table
+        self.watershed_table = dict()
         
+        # visited cells during process
+        nrows, ncols = self.d8_dir.shape
+        self.visited_cells = np.zeros((nrows,ncols), dtype=bool)
 
     # Returns cell coordinates for cells that drain into (x, y) of the D8 flow direction raster
     def _get_d8_neighbours(self, r: int, c: int) -> list:
@@ -148,6 +164,10 @@ class ASI:
                 continue
 
             n = self.d8_dir[nr, nc]
+            
+            # handle noData values
+            if n == self.nodata_value:
+                continue
 
             # get the vector value for the d8 number at n
             dn = self.direction_map[n]
@@ -289,36 +309,40 @@ class ASI:
         # Stack arguments: (row, column, watershed_id, outfall_id: may be none, ORDER (PRE=0, POST=1))
         # 'ORDER' is used to represent whether, for each cell visit, we should find children (i.e. get neighbours), or accumulate flow (flow cannot accumulate until we've reached the 'top' of the DEM)
 
+        # if accumulation has already been computed, abort
+        if self.d8_accum[r0, c0] != 0:
+            print("accumulation already computed, canceling")
+            return
+        
         PRE = 0
         POST = 1
         
-        nrows, ncols = self.d8_dir.shape
-        visited = np.zeros((nrows,ncols), dtype=bool)
-        
         stack = []
         # top of stack
-        stack.append((r0, c0, id, o_id, PRE))
+        stack.append((r0, c0, id, o_id, PRE, None))
         
         while stack:
-            r, c, id, o_id, phase = stack.pop()
+            r, c, id, o_id, phase, children = stack.pop()
             
             # pre visit
             if phase == PRE:
                 
                 # check if in visited
-                if visited[r, c]:
+                if self.visited_cells[r, c]:
                     continue
                 
-                visited[r,c] = True
+                self.visited_cells[r,c] = True
                 
+                children = self._get_d8_neighbours(r, c)
+                    
                 # push POST phase for next iteration
-                stack.append((r, c, id, o_id, POST))
+                stack.append((r, c, id, o_id, POST, children))
                 
                 # for every neighbour of (r,c)
-                for (i,j, c_o_id) in self._get_d8_neighbours(r, c):    
+                for (i,j, c_o_id) in children:    
                     # Append only if not visited
-                    if not visited[i,j]:
-                        stack.append((i, j, id, c_o_id, PRE))
+                    if not self.visited_cells[i,j]:
+                        stack.append((i, j, id, c_o_id, PRE, None))
 
                 # If cell (r,c) is an outfall:
                 if o_id is not None:
@@ -326,25 +350,22 @@ class ASI:
 
                         p, q = self.inlet_locations[i_id]
 
-                        # placeholder for a potentially better system later
-                        match self.watershed_type:
-                            case "OUTLET":
-                                w_id = id
-                            case "OUTFALL":
-                                w_id = o_id
-                            case "INLET":
-                                w_id = i_id
+                        w_id = {
+                            "OUTLET": id,
+                            "OUTFALL": o_id,
+                            "INLET":  i_id
+                        }[self.watershed_type]
 
                         # Push new inlet cell to stack including new watershed id (based on above)
-                        if not visited[p,q]:
-                            stack.append((p, q, w_id, None, PRE))
+                        if not self.visited_cells[p,q]:
+                            stack.append((p, q, w_id, None, PRE, None))
                 
             # post visit
             else:
                 
                 # normal cell flow
-                for ( i, j, _) in self._get_d8_neighbours(r, c):
-                    if visited[i,j]:
+                for ( i, j, _) in children:
+                    if self.visited_cells[i,j]:
                         self.d8_accum[r,c] += self.d8_accum[i,j] + 1
                 
                 # outfall-inlet flow
@@ -352,15 +373,11 @@ class ASI:
                     for i_id in self.inlet_connections[o_id]:
                         p, q = self.inlet_locations[i_id]
                         
-                        if visited[p,q]:
+                        if self.visited_cells[p,q]:
                             self.d8_accum[r,c] += self.d8_accum[p,q] + 1
                                 
                 # if this is the first time ID is added, create new entry in table
-                if id not in self.watershed_table:
-                    v = len(self.watershed_table)
-                    self.watershed_table[id] = v
-                else:
-                    v = self.watershed_table[id]
+                v = self.watershed_table.setdefault(id, len(self.watershed_table))
 
                 # assign watershed value
                 self.d8_watershed[r, c] = v
